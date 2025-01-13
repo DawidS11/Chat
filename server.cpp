38c9f69fb7a6280abe4bf44197a18855368f1ece
DawidS11 <dawidsmalcuga@gmail.com>
2025-01-13 18:19:15 +0100

    Create server addr

--- server.cpp
+++ server.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <sys/socket.h>
+#include <netinet/in.h>
 
 #include "values.hpp"
 
@@ -14,4 +15,12 @@ int main()
         std::cerr << "socket() error." << std::endl;
         exit(1);
     }
+
+    sockaddr_in server_addr;
+    std::memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(SERVER_PORT);
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    
 }
