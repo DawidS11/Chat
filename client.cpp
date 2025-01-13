05560afe588b967491680ec850c3c31d46727235
DawidS11 <dawidsmalcuga@gmail.com>
2025-01-13 19:04:40 +0100

    Implement client socket

--- client.cpp
+++ client.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
 #include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
 
 #include "values.hpp"
 
 int main ()
 {
     std::cout << "CLIENT" << std::endl;
+
+    int client_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (client_socket == -1)
+    {
+        // TODO: Error handling.
+        std::cerr << "Client: socket() error." << std::endl;
+        close(client_socket);
+        exit(1);
+    }
+
+    sockaddr_in server_addr;
+    std::memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(SERVER_PORT);
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 }
