7f94e851aaf90606f6b0f4453e048ba13de78258
DawidS11 <dawidsmalcuga@gmail.com>
2025-01-13 17:56:01 +0100

    Use socket()

--- client.cpp
+++ client.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <sys/socket.h>
 
 int main ()
 {
--- server.cpp
+++ server.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include <sys/socket.h>
 
 int main()
 {
     std::cout << "SERVER" << std::endl;
+
+    int server_socket = socket(AF_INET, SOCK_STREAM, 0);
 }
