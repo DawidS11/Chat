3892c752f699b6c83cdbc329a84f752fe2987ecd
DawidS11 <dawidsmalcuga@gmail.com>
2025-01-13 19:12:51 +0100

    Update ip address

--- client.cpp
+++ client.cpp
@@ -2,6 +2,7 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <unistd.h>
+#include <arpa/inet.h>
 
 #include "values.hpp"
 
@@ -22,7 +23,7 @@ int main ()
     std::memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(SERVER_PORT);
-    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_addr.sin_addr.s_addr = inet_addr(IP);
 
     if (connect(client_socket, reinterpret_cast<sockaddr*>(&server_addr), sizeof(server_addr)) == -1)
     {
--- values.hpp
+++ values.hpp
@@ -1,3 +1,4 @@
 
 #define SERVER_PORT 2048
 #define MAX_CLIENTS 1
+#define IP "127.0.0.1"
